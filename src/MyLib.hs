91b596d2e76164e6df77af88b538b8e19caaab22
sheepfleece <rowanmorrison@protonmail.ch>
2020-04-28 23:19:14 +0300

    Use MultiParamTypeClasses instead

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,16 +1,18 @@
-{-# LANGUAGE DataKinds, FlexibleInstances, MultiParamTypeClasses                  #-}
-{-# LANGUAGE GADTs, TypeApplications                      #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE PolyKinds                  #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE TypeOperators              #-}
-{-# LANGUAGE UndecidableInstances, RebindableSyntax       #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE RebindableSyntax      #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE StandaloneDeriving    #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE UndecidableInstances  #-}
 
 module MyLib () where
 
-import           Control.Exception      hiding (catches, throw, Handler)
+import           Control.Exception      hiding (Handler, catches, throw)
 import qualified Control.Exception      as E
 import           Control.Monad.IO.Class
 import           Data.Kind              (Constraint, Type)
@@ -41,9 +43,9 @@ type family (:++:) (a :: [k]) (b :: [k]) :: [k] where
   '[] :++: bs = bs
   (a:as) :++: bs = If (Member a bs) (as :++: bs) (a ': (as :++: bs))
 
-class MonadThrow (t :: [Type] -> (Type -> Type) -> (Type -> Type)) where
-  inject :: forall (es :: [Type]) m a. m a -> t es m a
-  strip :: t es m a -> m a 
+class MonadThrow (t :: [Type] -> (Type -> Type) -> (Type -> Type)) (m :: Type -> Type) where
+  inject :: forall (es :: [Type]) a . m a -> t es m a
+  strip :: t es m a -> m a
   stripIO :: t es m a -> IO a
 
   (>!=) :: (Monad m) => t es m a -> (a -> t es' m b) -> t (es :++: es') m b
@@ -51,36 +53,38 @@ class MonadThrow (t :: [Type] -> (Type -> Type) -> (Type -> Type)) where
   (>!) :: (Monad m) => t es m a -> t es' m b -> t (es :++: es') m b
   t >! m = t >!= \_ -> m
 
-throws :: forall (es :: [Type]) t m a . (MonadThrow t, Monad m) => m a -> t es m a 
+throws :: forall (es :: [Type]) t m a . (MonadThrow t m, Monad m) => m a -> t es m a
 throws = inject
 
-throw :: (MonadIO m, Exception e, MonadThrow t) => e -> t '[e] m a
+throw :: (MonadIO m, Exception e, MonadThrow t m) => e -> t '[e] m a
 throw = inject . liftIO . E.throwIO
 
 newtype Throws (es :: [Type]) (m :: Type -> Type) (a :: Type) where
   Throws :: m a -> Throws es m a
 
-instance MonadThrow Throws where
+instance MonadThrow Throws IO where
   inject = Throws
   (Throws m) >!= f = Throws $ m >>= (strip . f)
   strip (Throws m) = m
+  stripIO = strip
 
 
-data Handler e a = Exception e => Handler (e -> IO a)
+data Handler e a where
+  Handler :: Exception e => (e -> IO a) -> Handler e a
 
-infixr 5 :&&: 
+infixr 5 :&&:
 data Handlers (es :: [Type]) (a :: Type) :: Type where
   (:&&:) :: Handler e a -> Handlers es' a -> Handlers (e ': es') a
-  Nil :: Handlers '[] a 
+  Nil :: Handlers '[] a
   Only :: Handler e a -> Handlers '[e] a
 
 strip' :: Handlers es a -> [E.Handler a]
-strip' (Nil) = []
+strip' (Nil)               = []
 strip' (Handler f :&&: hs) = E.Handler f : strip' hs
-strip' (Only (Handler f)) = [E.Handler f]
+strip' (Only (Handler f))  = [E.Handler f]
 
-catches :: (MonadThrow t, Monad m) => t es m a -> Handlers es a -> IO a
-catches t hs = E.catches (stripIO t) (strip' hs) 
+catches :: (MonadThrow t m, Monad m) => t es m a -> Handlers es a -> IO a
+catches t hs = E.catches (stripIO t) (strip' hs)
 
 data ExampleException = Example1
     | Example2
@@ -109,7 +113,7 @@ hArith = Handler (putStrLn . show)
 hArray :: Handler ArrayException ()
 hArray = Handler (putStrLn . show)
 
-hLovely :: Handler LovelyException () 
+hLovely :: Handler LovelyException ()
 hLovely = Handler (putStrLn . show)
 
 hExample :: Handler ExampleException ()
